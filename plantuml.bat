2eb1bf2f2a5eaf0824ffdb8938429c34f374560b
Harkins <tmngoc1108@gmail.com>
2025-04-19 10:00:12 +0700

    Đăng lại ảnh

---
+++ plantuml.bat
@@ -0,0 +1,4 @@
+java -jar "plantuml-1.2025.0.jar" "sequence" ".\out\sequence"
+java -jar "plantuml-1.2025.0.jar" "usecase" ".\out\usecase"
+@REM java -jar "plantuml-1.2025.0.jar" "sequence" "./out/sequence"
+@REM java -jar "plantuml-1.2025.0.jar" "sequence" "./out/sequence"
